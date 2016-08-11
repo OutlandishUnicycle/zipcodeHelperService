46ef7c42c788f0fbcf3cc4450fec9b9131156313
Evan Moncuso <evan.moncuso@gmail.com>
2016-08-11 16:59:45 -0700

    Remove secret file

--- server/db/database.js
+++ server/db/database.js
@@ -1,6 +1,4 @@
 const Sequelize = require('sequelize');
-const secrets = require('../../secret.js');
-
 
 let db = new Sequelize(process.env.pgdb, process.env.pguser, process.env.pgpw, {
   dialect: 'postgres',
